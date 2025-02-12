d762f82346c5cda1f3f9bdca3c18b2aff184da68
Douglas <douglasmodolo@outlook.com.br>
2025-02-12 20:04:17 -0300

    Implementação do aplaudir com arrow function chamando a alteração do estado

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,12 +2,19 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css'
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment}) {
+
+    const [likeCount, setLikeCount] = useState(0);
 
     function handleDeleteComment() {
         onDeleteComment(content);
     }
+
+    // function handleLikeComment() {        
+    //     setLikeCount(likeCount + 1);
+    // }
 
     return (
         <div className={styles.comment}>
@@ -30,9 +37,9 @@ export function Comment({ content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={() => setLikeCount(likeCount + 1)}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
